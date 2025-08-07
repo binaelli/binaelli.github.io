83a088dcab22998eea2d12bff49efd3d911eccf3
binaelli <bina.elli@outlook.com>
2025-08-06 23:13:04 -0700

    commit

--- Transition Example/Script.js
+++
@@ -1,7 +0,0 @@
-const btn = document.getElementById('rotateBtn');
-const image = document.getElementById('image');
-
-btn.addEventListener('click', () => {
-  image.classList.toggle('rotate');
-});
-// This script toggles the 'rotate' class on the image when the button is clicked
